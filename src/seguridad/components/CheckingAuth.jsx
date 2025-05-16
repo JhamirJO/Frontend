2d53b4bba6b6027524e6b971e45c7f486f51bf36
JhamirJO <jhamir.justino@unmsm.edu.pe>
2025-05-15 21:00:18 -0500

    Login

--- src/seguridad/components/CheckingAuth.jsx
+++ src/seguridad/components/CheckingAuth.jsx
@@ -1,9 +1,9 @@
-import { CircularProgress, Grid2 } from '@mui/material';
+import { CircularProgress, Grid } from '@mui/material';
 
 
 export const CheckingAuth = () => {
   return (
-    <Grid2
+    <Grid
       container
       spacing={ 0 }
       direction="column"
@@ -11,12 +11,12 @@ export const CheckingAuth = () => {
       justifyContent="center"
       sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
     >
-        <Grid2 container
+        <Grid container
             direction='row'
             justifyContent='center'
             >
             <CircularProgress color='warning' />
-        </Grid2>
-    </Grid2>
+        </Grid>
+    </Grid>
   )
 }
